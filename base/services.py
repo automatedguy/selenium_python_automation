f45e94ae9e040c81559239e3cca04a007d6c5f10
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-02-14 18:21:10 -0300

    Step by Step will make this workls :D

--- base/services.py
+++ base/services.py
@@ -19,6 +19,58 @@ class Apikeys:
         return channel['value']
 
 
+class Autocomplete:
+    def __init__(self, place, entity_type):
+        self.autocomplete_url = APIST_ALMUNDO_COM \
+                                + '/api/autocomplete/suggestions/es?' \
+                                + 'q=' + place \
+                                + '&entityType=' + entity_type
+
+    def get_entity_id(self, apikey, location_name):
+        raw_autocomplete_suggestions = requests.get(self.autocomplete_url, headers={'X-Apikey': apikey})
+        json_autocomplete_suggestions = json.loads(raw_autocomplete_suggestions.text)
+        for entities in json_autocomplete_suggestions['suggestions'][0]['entities']:
+            if location_name in entities['label']:
+                print('Location Found:' + entities['label'])
+                break
+        return entities['id']
+
+
+class HotelsAvailabilities:
+    def __init__(self, entity_id, entity_type, checkin, checkout, room, language, site):
+        self.hotel_availabilities_url = APIST_ALMUNDO_COM \
+                                        + '/api/hotels/v2/availabilities?' \
+                                        + 'entityId=' + entity_id \
+                                        + '&entityType=' + entity_type \
+                                        + '&checkin=' + checkin \
+                                        + '&checkout=' + checkout \
+                                        + '&room=' + room \
+                                        + '&language=' + language \
+                                        + '&site=' + site
+
+    def get_hotel_id(self, apikey):
+        raw_hotels_availabilities = requests.get(self.hotel_availabilities_url, headers={'X-Apikey': apikey})
+        json_hotels_availabilities = json.loads(raw_hotels_availabilities.text)
+        return json_hotels_availabilities['availabilities'][0]['id']
+
+
+class HotelsDetails:
+    def __init__(self, hotel_id, checkin, checkout, room, language, site):
+        self.hotels_details_url = APIST_ALMUNDO_COM \
+                                    + '/api/hotels/v2/detail?' \
+                                    + 'hotelId=' + hotel_id \
+                                    + '&checkin=' + checkin \
+                                    + '&checkout=' + checkout \
+                                    + '&room=' + room \
+                                    + '&language=' + language \
+                                    + '&site=' + site
+
+    def get_hotel_id(self, apikey):
+        raw_hotel_details = requests.get(self.hotels_details_url, headers={'X-Apikey': apikey})
+        json_hotel_details = json.loads(raw_hotel_details.text)
+        return json_hotel_details
+
+
 class FlightsClusters:
     def __init__(self, origin, destination, departure_date, return_date, site, language, adults, children, infants):
         self.flights_clusters_url = APIST_ALMUNDO_COM \
@@ -59,9 +111,18 @@ apikeys = Apikeys()
 channel_apikey = apikeys.get_apikey()
 print('X-apikey: [' + channel_apikey + ']')
 
+# FLIGHTS THINGS
 flights_clusters = FlightsClusters('BUE', 'MIA', '2018-03-18', '2018-04-02', 'ARG', 'es', '2', '1', '0')
 product_id = flights_clusters.get_flight_id(channel_apikey)
 print('Flight ID: [' + product_id + ']')
+
+
+# HOTEL THINGS
+# autocomplete = Autocomplete('MIA', 'CITY')
+# location_entity_id = autocomplete.get_entity_id(channel_apikey, 'Miami')
+# print(location_entity_id)
+# hotels_availabilities = HotelsAvailabilities(location_entity_id, 'CITY', '2018-03-18', '2018-04-02', '2', 'es', 'ARG')
+# selected_hotel_id = hotels_availabilities.get_hotel_id(channel_apikey)
 
 
 cart = Cart('ARG', 'es')
