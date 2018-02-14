acb95c323ecaca4064d94a7660fd289500ab0d5b
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-02-14 13:21:39 -0300

    Fixed Declarations.

--- base/services.py
+++ base/services.py
@@ -33,9 +33,8 @@ class FlightsClusters:
 
         print('Flight Cluster URl: [' + self.flights_clusters_url + ']')
 
-    @classmethod
-    def get_flight_id(cls, apikey, flights_clusters_url):
-        raw_flights_clusters = requests.get(flights_clusters_url, headers={'X-Apikey': apikey})
+    def get_flight_id(self, apikey):
+        raw_flights_clusters = requests.get(self.flights_clusters_url, headers={'X-Apikey': apikey})
         json_flights_clusters = json.loads(raw_flights_clusters.text)
         return str(json_flights_clusters['clusters'][0]['segments'][0]['choices'][0]['id']) \
             + '*' + str(json_flights_clusters['clusters'][0]['segments'][1]['choices'][0]['id'])
@@ -49,10 +48,9 @@ class Cart:
                         + '&language=' + language
         print('Cart Book URL: [' + self.book_url + ']')
 
-    @classmethod
-    def get_cart_id(cls, apikey, flight_id, book_url):
+    def get_cart_id(self, apikey, flight_id):
         data = {"products": [{"type": "FLIGHT", "id": flight_id}]}
-        raw_cart_book_id = requests.post(book_url, headers={'X-Apikey': apikey}, json=data)
+        raw_cart_book_id = requests.post(self.book_url, headers={'X-Apikey': apikey}, json=data)
         json_cart_book_id = json.loads(raw_cart_book_id.text)
         return json_cart_book_id['cart_id']
 
@@ -62,10 +60,10 @@ channel_apikey = apikeys.get_apikey()
 print('X-apikey: [' + channel_apikey + ']')
 
 flights_clusters = FlightsClusters('BUE', 'MIA', '2018-03-18', '2018-04-02', 'ARG', 'es', '2', '1', '0')
-product_id = flights_clusters.get_flight_id(channel_apikey, flights_clusters.flights_clusters_url)
+product_id = flights_clusters.get_flight_id(channel_apikey)
 print('Flight ID: [' + product_id + ']')
 
 
 cart = Cart('ARG', 'es')
-cart_id = cart.get_cart_id(channel_apikey, product_id, cart.book_url)
+cart_id = cart.get_cart_id(channel_apikey, product_id)
 print('Cart ID: [' + cart_id + ']')
