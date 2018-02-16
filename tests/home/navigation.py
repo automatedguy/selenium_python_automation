92b712a107c12fb8cb6b3ac2a17ee6b7a6d71ac2
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-02-16 00:45:20 -0300

    Added some anotations

--- tests/home/navigation.py
+++ tests/home/navigation.py
@@ -5,13 +5,14 @@ from page import LoginModal
 
 
 class HomeNavigationTest(BaseTest):
-    def test_navigate_to_home(self):
-        pass
-
+    def setUp(self):
         login_modal = LoginModal(self.driver)
         login_modal.click_close_login_modal()
 
+    def test_navigate_to_home(self):
+        pass
 
+        
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(HomeNavigationTest)
     unittest.TextTestRunner(verbosity=2).run(suite)
